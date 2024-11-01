37f3395136165608b83925d70ded836274d90301
Ayesha-Aziz <moizs4644@gmail.com>
2024-11-02 04:59:02 +0500

    mileston_5

--- app.js
+++ app.js
@@ -1,71 +1,115 @@
 "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
-// // import jQuery from "jquery";
-// require("print-this");
-let resumeForm = document.querySelector("#cv-form");
-let resumeOutput = document.querySelector("#resume-output");
-let editBtn = document.querySelector("#edit-btn");
-let shareBtn = document.querySelector("#share-btn");
-let printBtn = document.querySelector("#print-btn");
-resumeForm?.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    resumeOutput.style.display = "block";
-    shareBtn.style.display = "block";
-    editBtn.style.display = "block";
-    printBtn.style.display = "block";
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const phone = document.getElementById("phone");
-    const education = document.getElementById("education");
-    const experience = document.getElementById("experience");
-    const skills = document.getElementById("skills");
-    if (!name || !email || !phone || !education || !experience || !skills) {
-        alert("please fill all field");
-        return;
+let cvform = document.querySelector("#cv-form");
+let skillBtn = document.querySelector("#skills-btn");
+let skillList = document.querySelector(".skills");
+let editbtn = document.getElementById("edit");
+let cvTemplate = document.getElementById("cv-template");
+let sharebtn = document.getElementById("share-btn");
+skillBtn.innerHTML = "Hide Skills";
+skillBtn?.addEventListener("click", () => {
+    if (skillList.style.display === "none" || !skillList.style.display) {
+        skillBtn.innerHTML = "Hide Skills";
+        skillList.style.display = "block";
     }
-    document.getElementById("resumeName").innerText = `Name: ${name.value}`;
-    document.getElementById("resumeEmail").innerText = `Email: ${email.value}`;
-    document.getElementById("resumePhone").innerText = `Phone: ${phone.value}`;
-    document.getElementById("resumeEducation").innerText = `Education: ${education.value}`;
-    document.getElementById("resumeExperience").innerText = `Experience: ${experience.value}`;
-    document.getElementById("resumeSkils").innerText = `Skills: ${skills.value}`;
+    else {
+        skillList.style.display = "none";
+        skillBtn.innerHTML = "Show Skills";
+    }
+});
+cvform?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let name = document.getElementById("nameField").value;
+    let currently_live = document.getElementById("currently_live").value;
+    let DOB = document.getElementById("dateField").value;
+    let phone = document.getElementById("contactField").value;
+    let email = document.getElementById("emailField").value;
+    let address = document.getElementById("addressField").value;
+    // School
+    const degree = document.getElementById("degree").value;
+    const institution = document.getElementById("institution").value;
+    const graduationDateStart = document.getElementById("graduation-date-start").value;
+    const graduationDateEnd = document.getElementById("graduation-date-end").value;
+    // College
+    // const degree2 = (document.getElementById("degree2") as HTMLInputElement).value;
+    // const institution2 = (document.getElementById("institution2") as HTMLInputElement).value;
+    // const collegeDateStart = (document.getElementById("college-end-date") as HTMLInputElement).value;
+    // const collegeDateEnd = (document.getElementById("college-start-date") as HTMLInputElement).value;
+    const jobTitle = document.getElementById("job-title").value;
+    const company = document.getElementById("company").value;
+    const startDate = document.getElementById("start-date").value;
+    const endDate = document.getElementById("end-date").value;
+    const jobDescription = document.getElementById("job-description").value;
+    // links
+    let Github = document.getElementById("GithubField").value;
+    let linkedin = document.getElementById("linkedinField").value;
+    document.getElementById("currentlyT").textContent = currently_live;
+    document.getElementById("DOBT").textContent = DOB;
+    document.getElementById("nameT").textContent = name;
+    document.getElementById("contactT").textContent = phone;
+    document.getElementById("emailT").textContent = email;
+    document.getElementById("addressT").textContent = address;
+    // School
+    document.getElementById("display-degree").textContent = degree;
+    document.getElementById("display-institution").textContent = institution;
+    document.getElementById("display-graduation-start").textContent = graduationDateStart;
+    document.getElementById("display-graduation-end").textContent = graduationDateEnd;
+    // College
+    // (document.getElementById("display-degree2") as HTMLElement).textContent = degree2;
+    // (document.getElementById("display-institution2") as HTMLElement).textContent = institution2;
+    // (document.getElementById("display-graduation-start2") as HTMLElement).textContent = collegeDateStart;
+    // (document.getElementById("display-graduation-end2") as HTMLElement).textContent = collegeDateEnd;
+    document.getElementById("LinkedInT").textContent = linkedin;
+    document.getElementById("githubT").textContent = Github;
+    document.getElementById("display-job-title").textContent = jobTitle;
+    document.getElementById("display-company").textContent = company;
+    document.getElementById("display-start-date").textContent = startDate;
+    document.getElementById("display-end-date").textContent = endDate;
+    document.getElementById("display-discription").textContent = jobDescription;
+    // ******* Skills *********
+    let skills = document.getElementsByClassName("sfield");
+    let skillsStr = "";
+    for (let e of skills) {
+        let entries = e.value.split(",");
+        for (let entry of entries) {
+            skillsStr += `<li>${entry.trim()}</li>`;
+        }
+    }
+    document.getElementById("skillT").innerHTML = skillsStr;
+    // **** Setting Image ****
+    let fileInput = document.getElementById("imageField").files?.[0];
+    if (fileInput) {
+        let reader = new FileReader();
+        reader.onloadend = () => document.getElementById("imgTemplate").src = reader.result;
+        reader.readAsDataURL(fileInput);
+    }
+    // **** Setting Image ****
+    let fileInput2 = document.getElementById("imageField").files?.[0];
+    if (fileInput2) {
+        let reader = new FileReader();
+        reader.onloadend = () => document.getElementById("imgTemplate-hide").src = reader.result;
+        reader.readAsDataURL(fileInput2);
+    }
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
+    let names = document.getElementById("nameField");
     // shareable link
-    shareBtn.addEventListener("click", async () => {
+    sharebtn.addEventListener("click", async () => {
         try {
-            const shareableLink = `https://milestone5-unique-path-and-shareable-link-ayesha.vercel.app/?username.value/${name.value.replace(/\s+/g, '_')}`;
+            const shareableLink = `https://milestone5-unique-path-and-shareable-link-ayesha.vercel.app/?username.value/${names.value.replace(/\s+/g, '_')}`;
             await navigator.clipboard.writeText(shareableLink);
             alert("Shareable linkcopied to Clipboard!");
+            console.log("hi");
         }
         catch (err) {
             alert("Failed to copy link clipboard . please try again!");
         }
     });
 });
-// Edit Resume button functionality
-editBtn?.addEventListener("click", () => {
-    resumeForm.style.display = "block"; // Show the form again
-    resumeOutput.style.display = "none"; // Hide the resume output
-    editBtn.style.display = "none"; // Hide the edit button
-    shareBtn.style.display = "none";
-    printBtn.style.display = "none";
-    // Repopulate the form with current resume data
-    const name = document.getElementById("resumeName")?.innerText.split(': ')[1];
-    const email = document.getElementById("resumeEmail")?.innerText.split(': ')[1];
-    const phone = document.getElementById("resumePhone")?.innerText.split(': ')[1];
-    const education = document.getElementById("resumeEducation")?.innerText;
-    const experience = document.getElementById("resumeExperience")?.innerText;
-    const skills = document.getElementById("resumeSkils")?.innerText;
-    // Set the form inputs to these values
-    document.getElementById("name").value = name ?? '';
-    document.getElementById("email").value = email ?? '';
-    document.getElementById("phone").value = phone ?? '';
-    document.getElementById("education").value = education ?? '';
-    document.getElementById("experience").value = experience ?? '';
-    document.getElementById("skills").value = skills ?? '';
-});
-// download pdf
-$(document).ready(function () {
-    $("#print-btn").on("click", function () {
-        $("#resume-output").printThis();
-    });
+function printCV() {
+    window.print();
+}
+editbtn?.addEventListener("click", () => {
+    cvform.style.display = "block";
+    cvTemplate.style.display = "none";
 });
--- app.ts
+++ app.ts
@@ -1,90 +1,152 @@
-// import jQuery from "jquery";
-import "print-this";
-
-let resumeForm = document.querySelector("#cv-form") as HTMLElement;
-let resumeOutput = document.querySelector("#resume-output") as HTMLInputElement;
-let editBtn = document.querySelector("#edit-btn") as HTMLElement;
-let shareBtn = document.querySelector("#share-btn") as HTMLElement;
-let printBtn = document.querySelector("#print-btn") as HTMLElement;
-
-resumeForm?.addEventListener("submit", (evt) => {
-    evt.preventDefault()
-    resumeOutput.style.display = "block"
-    shareBtn.style.display = "block"
-    editBtn.style.display = "block"
-    printBtn.style.display = "block"
-
-    const name = document.getElementById("name") as HTMLInputElement;
-    const email = document.getElementById("email") as HTMLInputElement;
-    const phone = document.getElementById("phone") as HTMLInputElement;
-    const education = document.getElementById("education") as HTMLInputElement;
-    const experience = document.getElementById("experience") as HTMLInputElement;
-    const skills = document.getElementById("skills") as HTMLInputElement;
+let cvform = document.querySelector("#cv-form") as HTMLFormElement;
+let skillBtn = document.querySelector("#skills-btn") as HTMLButtonElement;
+let skillList = document.querySelector(".skills") as HTMLElement;
+let editbtn = document.getElementById("edit") as HTMLElement;
+let cvTemplate = document.getElementById("cv-template") as HTMLFormElement;
+let sharebtn = document.getElementById("share-btn") as HTMLElement;
 
 
-    if (!name || !email || !phone || !education || !experience || !skills) {
-        alert("please fill all field");
-        return;
+
+skillBtn.innerHTML = "Hide Skills";
+
+skillBtn?.addEventListener("click", () => {
+  if (skillList.style.display === "none" || !skillList.style.display) {
+    skillBtn.innerHTML = "Hide Skills"
+    skillList.style.display = "block"
+  }
+  else {
+    skillList.style.display = "none"
+    skillBtn.innerHTML = "Show Skills"
+  }
+});
+
+
+cvform?.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let name = (document.getElementById("nameField") as HTMLInputElement).value;
+  let currently_live = (document.getElementById("currently_live") as HTMLInputElement).value;
+  let DOB = (document.getElementById("dateField") as HTMLInputElement).value;
+  let phone = (document.getElementById("contactField") as HTMLInputElement).value;
+  let email = (document.getElementById("emailField") as HTMLInputElement).value;
+  let address = (document.getElementById("addressField") as HTMLInputElement).value;
+
+  // School
+  const degree = (document.getElementById("degree") as HTMLInputElement).value;
+  const institution = (document.getElementById("institution") as HTMLInputElement).value;
+  const graduationDateStart = (document.getElementById("graduation-date-start") as HTMLInputElement).value;
+  const graduationDateEnd = (document.getElementById("graduation-date-end") as HTMLInputElement).value;
+
+  // College
+  // const degree2 = (document.getElementById("degree2") as HTMLInputElement).value;
+  // const institution2 = (document.getElementById("institution2") as HTMLInputElement).value;
+  // const collegeDateStart = (document.getElementById("college-end-date") as HTMLInputElement).value;
+  // const collegeDateEnd = (document.getElementById("college-start-date") as HTMLInputElement).value;
+
+
+  const jobTitle = (document.getElementById("job-title") as HTMLInputElement).value;
+  const company = (document.getElementById("company") as HTMLInputElement).value;
+  const startDate = (document.getElementById("start-date") as HTMLInputElement).value;
+  const endDate = (document.getElementById("end-date") as HTMLInputElement).value;
+  const jobDescription = (document.getElementById("job-description") as HTMLInputElement).value;
+
+
+  // links
+  let Github = (document.getElementById("GithubField") as HTMLInputElement).value;
+  let linkedin = (document.getElementById("linkedinField") as HTMLInputElement).value;
+
+
+
+  (document.getElementById("currentlyT") as HTMLElement).textContent = currently_live;
+  (document.getElementById("DOBT") as HTMLElement).textContent = DOB;
+  (document.getElementById("nameT") as HTMLElement).textContent = name;
+  (document.getElementById("contactT") as HTMLElement).textContent = phone;
+  (document.getElementById("emailT") as HTMLElement).textContent = email;
+  (document.getElementById("addressT") as HTMLElement).textContent = address;
+
+  // School
+  (document.getElementById("display-degree") as HTMLElement).textContent = degree;
+  (document.getElementById("display-institution") as HTMLElement).textContent = institution;
+  (document.getElementById("display-graduation-start") as HTMLElement).textContent = graduationDateStart;
+  (document.getElementById("display-graduation-end") as HTMLElement).textContent = graduationDateEnd;
+
+  // College
+  // (document.getElementById("display-degree2") as HTMLElement).textContent = degree2;
+  // (document.getElementById("display-institution2") as HTMLElement).textContent = institution2;
+  // (document.getElementById("display-graduation-start2") as HTMLElement).textContent = collegeDateStart;
+  // (document.getElementById("display-graduation-end2") as HTMLElement).textContent = collegeDateEnd;
+
+
+
+
+  (document.getElementById("LinkedInT") as HTMLElement).textContent = linkedin;
+  (document.getElementById("githubT") as HTMLElement).textContent = Github;
+
+  (document.getElementById("display-job-title") as HTMLElement).textContent = jobTitle;
+  (document.getElementById("display-company") as HTMLElement).textContent = company;
+  (document.getElementById("display-start-date") as HTMLElement).textContent = startDate;
+  (document.getElementById("display-end-date") as HTMLElement).textContent = endDate;
+  (document.getElementById("display-discription") as HTMLElement).textContent = jobDescription;
+
+
+  // ******* Skills *********
+  let skills = document.getElementsByClassName("sfield") as HTMLCollectionOf<HTMLInputElement>;
+  let skillsStr = "";
+
+  for (let e of skills) {
+    let entries = e.value.split(",");
+
+    for (let entry of entries) {
+      skillsStr += `<li>${entry.trim()}</li>`;
     }
-    (document.getElementById("resumeName") as HTMLElement).innerText = `Name: ${name.value}`;
-    (document.getElementById("resumeEmail") as HTMLElement).innerText = `Email: ${email.value}`;
-    (document.getElementById("resumePhone") as HTMLElement).innerText = `Phone: ${phone.value}`;
-    (document.getElementById("resumeEducation") as HTMLElement).innerText = `Education: ${education.value}`;
-    (document.getElementById("resumeExperience") as HTMLElement).innerText = `Experience: ${experience.value}`;
-    (document.getElementById("resumeSkils") as HTMLElement).innerText = `Skills: ${skills.value}`;
+  }
+  (document.getElementById("skillT") as HTMLElement).innerHTML = skillsStr;
+
+  // **** Setting Image ****
+  let fileInput = (document.getElementById("imageField") as HTMLInputElement).files?.[0];
+  if (fileInput) {
+    let reader = new FileReader();
+    reader.onloadend = () => (document.getElementById("imgTemplate") as HTMLImageElement).src = reader.result as string;
+    reader.readAsDataURL(fileInput);
+  }
+
+  // **** Setting Image ****
+  let fileInput2 = (document.getElementById("imageField") as HTMLInputElement).files?.[0];
+  if (fileInput2) {
+    let reader = new FileReader();
+    reader.onloadend = () => (document.getElementById("imgTemplate-hide") as HTMLImageElement).src = reader.result as string;
+    reader.readAsDataURL(fileInput2);
+  }
+
+  (document.getElementById("cv-form") as HTMLElement).style.display = "none";
+  (document.getElementById("cv-template") as HTMLElement).style.display = "block";
 
 
+  let names = document.getElementById("nameField") as HTMLInputElement;
 
-    // shareable link
-    shareBtn.addEventListener("click", async () => {
-        try {
-            const shareableLink = `https://milestone5-unique-path-and-shareable-link-ayesha.vercel.app/?username.value/${name.value.replace(/\s+/g, '_')}`
+  // shareable link
+  sharebtn.addEventListener("click", async () => {
+    try {
+      const shareableLink = `https://milestone5-unique-path-and-shareable-link-ayesha.vercel.app/?username.value/${names.value.replace(/\s+/g, '_')}`
 
-            await navigator.clipboard.writeText(shareableLink)
-            alert("Shareable linkcopied to Clipboard!")
+      await navigator.clipboard.writeText(shareableLink)
+      alert("Shareable linkcopied to Clipboard!")
 
-        } catch (err) {
-            alert("Failed to copy link clipboard . please try again!")
-        }
+    } catch (err) {
+      alert("Failed to copy link clipboard . please try again!")
+    }
 
-    })
+  })
+
+});
+
+function printCV() {
+  window.print();
+}
+
+editbtn?.addEventListener("click", () => {
+  cvform.style.display = "block";
+  cvTemplate.style.display = "none"
+
+
 })
-
-
-
-
-// Edit Resume button functionality
-editBtn?.addEventListener("click", () => {
-
-    resumeForm.style.display = "block"; // Show the form again
-    resumeOutput.style.display = "none"; // Hide the resume output
-    editBtn.style.display = "none"; // Hide the edit button
-    shareBtn.style.display = "none";
-    printBtn.style.display = "none";
-
-
-    // Repopulate the form with current resume data
-    const name = document.getElementById("resumeName")?.innerText.split(': ')[1];
-    const email = document.getElementById("resumeEmail")?.innerText.split(': ')[1];
-    const phone = document.getElementById("resumePhone")?.innerText.split(': ')[1];
-    const education = document.getElementById("resumeEducation")?.innerText;
-    const experience = document.getElementById("resumeExperience")?.innerText;
-    const skills = document.getElementById("resumeSkils")?.innerText;
-
-    // Set the form inputs to these values
-    (document.getElementById("name") as HTMLInputElement).value = name ?? '';
-    (document.getElementById("email") as HTMLInputElement).value = email ?? '';
-    (document.getElementById("phone") as HTMLInputElement).value = phone ?? '';
-    (document.getElementById("education") as HTMLInputElement).value = education ?? '';
-    (document.getElementById("experience") as HTMLInputElement).value = experience ?? '';
-    (document.getElementById("skills") as HTMLInputElement).value = skills ?? '';
-
-});
-
-
-// download pdf
-$(document).ready(function () {
-    $("#print-btn").on("click", function () {
-        $("#resume-output").printThis();
-    });
-});
